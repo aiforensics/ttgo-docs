d753777e7c6e198dd15081ad7c1f57dd4863d017
Claudio <claudio@tracking.exposed>
2023-07-02 23:05:35 +0200

    explicit absolute link should go there

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/">
+            to="https://ttgo.trex.zone">
             Go back to the map️
           </Link>
         </div>
